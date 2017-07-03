fcf4bb3b8b07b793d803c55537232238187e71e5
CA_Quandt <alishaq@umich.edu>
2017-07-03 14:52:04 -0400

    Create Example_of_bash_PBS_script_creator.sh

---
+++ RAxML/Example_of_bash_PBS_script_creator.sh
@@ -0,0 +1,17 @@
+for file in 
+do
+echo " 
+#PBS -N raxml_$file
+#PBS -V
+#PBS -A lsa_flux
+#PBS -l qos=flux
+#PBS -q flux
+#PBS -l nodes=12:ppn=5,pmem=1gb,walltime=15:00:00
+#PBS -M @umich.edu
+#PBS -m abe
+#PBS -j oe
+
+#Your Code Goes Below:
+cd 
+mpirun -np 60 raxmlHPC-MPI -f a -s $file -n $file_raxml.nex -m PROTGAMMAAUTO -x 25678 -N 500 -p 8762" > $file.pbs
+done
