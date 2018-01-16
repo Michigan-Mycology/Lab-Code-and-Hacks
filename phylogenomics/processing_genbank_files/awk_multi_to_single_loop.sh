2c3cd147048eefc86877181f73cfec248d30ccd6
William J Davis <wjdavis90@gmail.com>
2018-01-16 15:48:13 -0500

    Update awk_multi_to_single_loop.sh

--- phylogenomics/processing_genbank_files/awk_multi_to_single_loop.sh
+++ phylogenomics/processing_genbank_files/awk_multi_to_single_loop.sh
@@ -1,7 +1,7 @@
-for file in [your folder]/*
+for file in [your folder]/*_penultimate_names.aa.fasta
 do
 let count ++
-m= ${file//}
+m= ${file/_penultimate_names.aa.fasta/}
 cat $file | awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);} END {printf("\n");}' > "$m"_final_names.aa.fasta
 done
 echo "All the single ladies! All the single ladies! Put your hands up!"
