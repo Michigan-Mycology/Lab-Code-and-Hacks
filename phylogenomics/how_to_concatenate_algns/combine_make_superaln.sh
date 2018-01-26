503ccfe0a404786a336029a12306a425cf8493aa
William J Davis <wjdavis90@gmail.com>
2018-01-26 12:12:38 -0500

    Update combine_make_superaln.sh

--- phylogenomics/how_to_concatenate_algns/combine_make_superaln.sh
+++ phylogenomics/how_to_concatenate_algns/combine_make_superaln.sh
@@ -1,4 +1,4 @@
 if [ ! -f expected ]; then
  bash your_path/jobs/make_expected_file.sh
 fi
-perl your_path/combine_fasaln.pl -o your_concatenated_algn -of fasta -d aln_final -expected expected
+perl your_path/combine_fasaln.pl -o your_concatenated_algn -of fasta -d aln_final -expected expected &> your_concatenated_algn.log
