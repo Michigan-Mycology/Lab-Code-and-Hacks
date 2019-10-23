ba78162926a56839dda469b1c0e28819beff51e6
amsesk <amsesk@umich.edu>
2019-10-23 11:26:17 -0400

    added some scripts to go with sop

---
+++ Computing_SOPs/genomics_scripts/remove_small_contigs.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+my $minlen = shift or die "Error: `minlen` parameter not provided\n";
+{
+    local $/=">";
+    while(<>) {
+        chomp;
+        next unless /\w/;
+        s/>$//gs;
+        my @chunk = split /\n/;
+        my $header = shift @chunk;
+        my $seqlen = length join "", @chunk;
+        print ">$_" if($seqlen >= $minlen);
+    }
+    local $/="\n";
+}
