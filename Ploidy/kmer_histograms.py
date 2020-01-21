b9b96a6b2c9f1438ee56b8f39efca62f2d4b3522
amsesk <amsesk@umich.edu>
2020-01-21 15:05:34 -0500

    Add files via upload

---
+++ Ploidy/kmer_histograms.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+# On krapbook with Tim's known haploid/diploid yeasts
+import plotly.express as px
+import plotly
+import pandas as pd
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+requiredNamed = parser.add_argument_group('required named arguments')
+requiredNamed.add_argument("-k", "--khist", metavar="khist_file", required=True, action="store", help="khist file from kmercountexact.sh")
+requiredNamed.add_argument("-p", "--peaks",  metavar="peaks_file", required=True, action="store", help="peaks file from kmercountexact.sh")
+args = parser.parse_args()
+
+hist = args.khist
+peaks = args.peaks
+
+prefix = os.path.split(hist)[1].split(".")[0] 
+
+hist = pd.read_csv(hist, sep = "\t")
+peaks = pd.read_csv(peaks, sep="\t", skiprows=range(0,13))
+
+fig = px.line(hist, x="#Depth", y="Count", title="Paraphysoderma sp. (diploid)")
+fig.update_xaxes(range=[0,600], title = "Depth")
+fig.update_yaxes(range=[0,peaks["max"].max()*1.25])
+
+#fig.show()
+plotly.io.orca.config.executable = '/usr/local/bin/orca'
+fig.write_image(file=f"{prefix}_kmers.pdf", format="pdf")
+
